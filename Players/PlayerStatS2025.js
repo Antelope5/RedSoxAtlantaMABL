e2f250e358e65aba9691ac3f88bcf2eef9b0add4
Antelope5 <123395479+Antelope5@users.noreply.github.com>
2025-04-06 21:02:24 -0400

    Create PlayerStatS2025.js

---
+++ Players/PlayerStatS2025.js
@@ -0,0 +1,221 @@
+// {'Game': ,'PA' : ,'AB' :  , 'R' :  , 'H' : , 'Single' : , 'Double' : , 'Triple': , 'HR' : , 'RBI' : , 'HBP': , 'BB' : , 'K' : , 'SB' :  , 'CS' :   },
+
+var AlexanderVStats= [
+    {'Game': "vs Senators" ,'PA' : 4,'AB' :  3, 'R' :  0, 'H' : 0, 'Single' : 0, 'Double' : 0, 'Triple': 0, 'HR' : 0, 'RBI' : 0, 'HBP': 0, 'BB' : 1, 'K' : 1, 'SB' :  0, 'CS' :  0 },
+
+
+]
+
+var AnthonyStats = [
+    
+
+    
+
+]
+
+var BrandinNixon =[
+    
+
+]
+
+var BrendanLarkin = [
+    {'Game': "vs Senators" ,'PA' : 4,'AB' :  1, 'R' :  3, 'H' : 1, 'Single' : 1, 'Double' : 0, 'Triple': 0, 'HR' : 0, 'RBI' : 0, 'HBP': 1, 'BB' : 2, 'K' : 0, 'SB' : 0 , 'CS' : 0},
+
+    
+
+]
+
+var EnyerPerozo =[
+    
+
+] 
+
+var JulioStats = [
+    {'Game': "vs Senators" ,'PA' : 4,'AB' :  4, 'R' :  1, 'H' : 4, 'Single' : 3, 'Double' : 1, 'Triple': 0, 'HR' : 0, 'RBI' : 0, 'HBP': 0, 'BB' : 0, 'K' : 0, 'SB' :  0, 'CS' : 0 },
+
+
+    
+
+]
+
+var KevinAlvarez =[
+    
+
+
+
+]
+
+var LennyPeralta = [
+  {'Game': "vs Senators" ,'PA' : 4,'AB' :  2, 'R' :  1, 'H' : 1, 'Single' : 0, 'Double' : 0, 'Triple': 0, 'HR' : 1, 'RBI' : 3, 'HBP': 0, 'BB' : 1, 'K' : 1, 'SB' :  0, 'CS' :  0},    
+
+
+
+]
+
+var LuisMoronta =[
+    {'Game': "vs Senators" ,'PA' : 4,'AB' :  4, 'R' : 1 , 'H' : 1, 'Single' : 1, 'Double' : 0, 'Triple': 0, 'HR' : 0, 'RBI' : 0, 'HBP': 0, 'BB' : 0, 'K' : 1, 'SB' :  0, 'CS' :  0 },
+
+
+
+]
+var LuisBaroni =[
+    {'Game': "vs Senators" ,'PA' : 4,'AB' :  4, 'R' :  0, 'H' : 0, 'Single' : 0, 'Double' : 0, 'Triple': 0, 'HR' : 0, 'RBI' : 0, 'HBP': 0, 'BB' : 0, 'K' : 3, 'SB' :  0, 'CS' :  0},
+
+
+
+
+]
+
+var LuisMartinez = [
+    
+
+
+
+]
+
+var LuisMunoz = [
+   
+
+
+
+]
+
+var ServioRincon =[
+    
+
+
+]
+
+var LeswardSuit =[
+  {'Game': "vs Senators" ,'PA' : 3,'AB' :  3, 'R' :  0, 'H' : 0, 'Single' : 0, 'Double' : 0, 'Triple': 0, 'HR' :0 , 'RBI' :0 , 'HBP':0 , 'BB' :0 , 'K' : 0, 'SB' : 0 , 'CS' :  0 },
+  
+]
+
+var TylerKendrick =[
+  {'Game': "vs Senators" ,'PA' : 4,'AB' :  4, 'R' :  0, 'H' : 2, 'Single' : 2, 'Double' : 0, 'Triple': 0, 'HR' : 0, 'RBI' : 1, 'HBP': 0, 'BB' : 0, 'K' : 1, 'SB' :  0, 'CS' : 0  },
+  
+]
+
+var HamptonMamay =[
+  {'Game': "vs Senators" ,'PA' : 3,'AB' :  3, 'R' :  1, 'H' : 0, 'Single' : 0, 'Double' : 0, 'Triple': 0, 'HR' : 0, 'RBI' : 0, 'HBP': 0, 'BB' : 0, 'K' : 0, 'SB' : 0 , 'CS' :  0 },
+  
+]
+
+var Dario =[
+  
+  
+]
+
+var BobbyBoniila =[
+  
+]
+
+//comment
+function addStats(stats){
+    //0
+   var PAsum = 0; // 0
+   var ABsum = 0; // 1
+   var Rsum = 0; // 2
+   var Hsum = 0; //3
+   var Singlesum = 0; //4
+   var Doublesum = 0; //5
+   var Triplesum = 0; //6
+   var HRsum = 0; //7
+   var RBIsum = 0; //8
+   var HBPsum = 0; //9
+   var BBsum = 0; //10
+   var Ksum = 0; // 11
+   var SBsum = 0; //12
+   var CSsum = 0; //13
+
+   for(var i = 0; i < stats.length; i++){
+       PAsum += stats[i].PA;
+       ABsum += stats[i].AB;
+       Rsum += stats[i].R;
+       Hsum += stats[i].H;
+       Singlesum += stats[i].Single;
+       Doublesum += stats[i].Double;
+       Triplesum += stats[i].Triple;
+       HRsum += stats[i].HR;
+       RBIsum += stats[i].RBI;
+       HBPsum += stats[i].HBP;
+       BBsum += stats[i].BB;
+       Ksum += stats[i].K;
+       SBsum += stats[i].SB;
+       CSsum += stats[i].CS;
+   }
+   var totalArr = [PAsum,ABsum,Rsum,Hsum,Singlesum,Doublesum,Triplesum,HRsum,RBIsum,HBPsum,BBsum,Ksum,SBsum,CSsum]; 
+ return totalArr;
+}
+
+
+function calculateStats(data){
+    var AVG;
+    var OBP;
+    var SLG;
+    var OPS;
+    var currentStats = data;
+
+    if (currentStats[1] == 0){
+        AVG = 0.000;
+        OBP = (currentStats[3]+currentStats[10]+currentStats[9])/(currentStats[1]+currentStats[10]+currentStats[9]);
+        SLG = 0.000;
+        OPS = OBP + SLG;
+    }
+    else{
+    AVG = (currentStats[3]/currentStats[1]);
+    OBP = (currentStats[3]+currentStats[10]+currentStats[9])/(currentStats[1]+currentStats[10]+currentStats[9]);
+    SLG = (currentStats[4]+(currentStats[5]*2)+(currentStats[6]*3)+(currentStats[7]*4))/currentStats[1];
+    OPS = OBP + SLG
+    }
+    var fullStats = [AVG.toFixed(3),OBP.toFixed(3),SLG.toFixed(3),OPS.toFixed(3)];
+
+    return fullStats
+
+
+}
+
+
+function buildTable(data){
+    var table = document.getElementById('PlayerTableS2025')
+
+    for(var i = 0; i<data.length; i++){
+        var row =`<tr>
+                    <td>${data[i].Game}</td>
+                    <td>${data[i].PA}</td>
+                    <td>${data[i].AB}</td>
+                    <td>${data[i].R}</td>
+                    <td>${data[i].H}</td>
+                    <td>${data[i].Single}</td>
+                    <td>${data[i].Double}</td>
+                    <td>${data[i].Triple}</td>
+                    <td>${data[i].HR}</td>
+                    <td>${data[i].RBI}</td>
+                    <td>${data[i].HBP}</td>
+                    <td>${data[i].BB}</td>
+                    <td>${data[i].K}</td>
+                    <td>${data[i].SB}</td>
+                    <td>${data[i].CS}</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>        
+                    </tr>`
+                    table.innerHTML += row
+    }
+    var dataArray = addStats(data);
+        var lastRow ='<tr><td>Total</td>';
+
+    for (var i = 0; i<dataArray.length; i++){
+        lastRow += `<td>${dataArray[i]}</td>`
+    }
+
+    var stats = calculateStats(dataArray);
+    for (var i =0; i<stats.length;i++){
+        lastRow += `<td>${stats[i]}</td>`
+    }
+
+        lastRow += `</tr>`
+        table.innerHTML += lastRow
+
+}
