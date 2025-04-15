bcfac8a642aa13efc16f8ab236ea0dafe99444ed
Antelope5 <123395479+Antelope5@users.noreply.github.com>
2025-04-14 22:10:27 -0400

    Update StatsS2025.js

--- src/StatsS2025.js
+++ src/StatsS2025.js
@@ -98,7 +98,7 @@ var LeswardSuit =[
 
 var TylerKendrick =[
   {'PA' : 4,'AB' :  4, 'R' :  0, 'H' : 2, 'Single' : 2, 'Double' : 0, 'Triple': 0, 'HR' : 0, 'RBI' : 1, 'HBP': 0, 'BB' : 0, 'K' : 1, 'SB' :  0, 'CS' : 0  },
-  {'PA' : ,'AB' :  , 'R' :  , 'H' : , 'Single' : , 'Double' : , 'Triple': , 'HR' : , 'RBI' : , 'HBP': , 'BB' : , 'K' : , 'SB' :  , 'CS' :   },
+  {'PA' : 1,'AB' :  1, 'R' :  0, 'H' : 1, 'Single' : 1, 'Double' : 0, 'Triple': 0, 'HR' : 0, 'RBI' : 1, 'HBP': 0, 'BB' : 0, 'K' : 0, 'SB' :  1, 'CS' :  0 },
   
 ]
 
