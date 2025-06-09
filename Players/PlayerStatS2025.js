6fb30792a9fcab5ca22140423985937b4c7e18d1
Antelope5 <123395479+Antelope5@users.noreply.github.com>
2025-06-08 20:59:18 -0400

    Update PlayerStatS2025.js

--- Players/PlayerStatS2025.js
+++ Players/PlayerStatS2025.js
@@ -76,7 +76,7 @@ var LuisMoronta =[
     {'Game': "vs Cyclones",'PA' : 4,'AB' :  4, 'R' :  2, 'H' : 2, 'Single' : 2, 'Double' : 0, 'Triple': 0, 'HR' : 0, 'RBI' : 0, 'HBP': 0, 'BB' : 0, 'K' : 0, 'SB' :  0, 'CS' :  0 },
     {'Game': "vs Stripers",'PA' : 5,'AB' :  5, 'R' :  1, 'H' : 1, 'Single' : 1, 'Double' : 0, 'Triple': 0, 'HR' : 0, 'RBI' : 0, 'HBP': 0, 'BB' : 0, 'K' : 1, 'SB' :  0, 'CS' :  0 },
     {'Game':"vs Bambinos", 'PA' : 5,'AB' :  5, 'R' :  1, 'H' : 2, 'Single' : 1, 'Double' : 1, 'Triple': 0, 'HR' : 0, 'RBI' : 2, 'HBP': 0, 'BB' : 0, 'K' : 0, 'SB' :  0, 'CS' : 0  },
-    {'Game':"vs Tortugas", 'PA' : 4,'AB' :  4, 'R' :  1, 'H' : 1, 'Single' : 1, 'Double' : 0, 'Triple': 0, 'HR' : 0, 'RBI' : 0, 'HBP': 0, 'BB' : 0, 'K' : 0, 'SB' :  0, 'CS' :  0 },
+    {'Game':"vs Tortugas", 'PA' : 4,'AB' :  4, 'R' :  1, 'H' : 1, 'Single' : 1, 'Double' : 0, 'Triple': 0, 'HR' : 0, 'RBI' : 0, 'HBP': 0, 'BB' : 0, 'K' : 0, 'SB' :  1, 'CS' :  0 },
 
 
 
