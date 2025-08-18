38a5caa8acd3efa7594e8e8046f4b23334b99ec7
Antelope5 <123395479+Antelope5@users.noreply.github.com>
2025-08-18 00:26:45 -0400

    Update PlayerStatS2025.js

--- Players/PlayerStatS2025.js
+++ Players/PlayerStatS2025.js
@@ -18,6 +18,8 @@ var AlexanderVStats= [
 ]
 
 var AnthonyStats = [
+    {'Game':"vs D Backs",'PA' : 3,'AB' :  2, 'R' :  3, 'H' : 2, 'Single' : 2, 'Double' : 0, 'Triple': 0, 'HR' : 0, 'RBI' : 0, 'HBP': 0, 'BB' : 1, 'K' : 0, 'SB' :  0, 'CS' : 0  },
+    {'Game':"vs Rain Makers",'PA' : 2,'AB' :  2, 'R' :  0, 'H' : 1, 'Single' : 0, 'Double' : 1, 'Triple': 0, 'HR' : 0, 'RBI' : 1, 'HBP': 0, 'BB' : 0, 'K' : 0, 'SB' :  0, 'CS' : 0  },
     
 
     
