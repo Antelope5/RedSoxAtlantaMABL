b98ebd9325ff8701426f9ec16d5f9a606000fd4b
Antelope5 <123395479+Antelope5@users.noreply.github.com>
2026-06-17 00:56:26 -0400

    Update Stats2026.js

--- src/Stats2026.js
+++ src/Stats2026.js
@@ -80,7 +80,7 @@ var LennyPeralta = [
      {'PA' : 4, 'AB' :  3, 'R' :  1, 'H' : 0, 'Single' : 0, 'Double' : 0, 'Triple': 0, 'HR' : 0, 'RBI' : 0, 'HBP': 0, 'BB' : 1, 'K' : 1, 'SB' :  0, 'CS' : 0  },
      {'PA' : 4, 'AB' :  4, 'R' :  2, 'H' : 2, 'Single' : 0, 'Double' : 1, 'Triple': 0, 'HR' : 1, 'RBI' : 2, 'HBP': 0, 'BB' : 0, 'K' : 1, 'SB' :  0, 'CS' :  0 },
      {'PA' : 4, 'AB' :  4, 'R' :  2, 'H' : 2, 'Single' : 2, 'Double' : 0, 'Triple': 0, 'HR' : 0, 'RBI' : 0, 'HBP': 0, 'BB' : 0, 'K' : 1, 'SB' :  0, 'CS' :  0 },
-     {'PA' : 4, 'AB' :  3, 'R' :  3, 'H' : 3, 'Single' : 1, 'Double' : 1, 'Triple': 0, 'HR' : 0, 'RBI' : 4, 'HBP': 0, 'BB' : 1, 'K' : 0, 'SB' :  0, 'CS' :  0 },
+     {'PA' : 4, 'AB' :  3, 'R' :  3, 'H' : 3, 'Single' : 1, 'Double' : 1, 'Triple': 0, 'HR' : 1, 'RBI' : 4, 'HBP': 0, 'BB' : 1, 'K' : 0, 'SB' :  0, 'CS' :  0 },
     
 
 
